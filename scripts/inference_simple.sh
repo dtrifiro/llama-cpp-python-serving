daaabbdde8bf2ed490cd243bd2d0bc40ce2f9ed5
Daniele Trifirò <dtrifiro@redhat.com>
2024-02-20 10:34:20 +0100

    scripts: cleanup inference_simple

--- scripts/inference_simple.sh
+++ scripts/inference_simple.sh
@@ -9,10 +9,10 @@ set -euxo pipefail
 
 ISVC_URL=$(kubectl get isvc llama-cpp-python -o jsonpath='{.status.components.predictor.url}')
 
-curl -k \
+curl -sk \
 	-H 'Content-type: application/json' \
 	-d '{
         "prompt": "'"${text}"'",
         "stop": "\n"
     }' \
-	"${ISVC_URL}/v1/completions" | jq
+	"${ISVC_URL}/v1/completions"
